869b927851bbb6539f40f814657a9d1b70b5f5cc
garett95 <7934650@gmail.com>
2022-12-06 16:28:55 +0300

    Add Details

--- MVC_Store/MVC_Store/Areas/Admin/Controllers/PagesController.cs
+++ MVC_Store/MVC_Store/Areas/Admin/Controllers/PagesController.cs
@@ -32,6 +32,7 @@ namespace MVC_Store.Areas.Admin.Controllers
         {
             return View();
         }
+
         // POST: Admin/Pages/AddPage
         [HttpPost]
         public ActionResult AddPage(PageVM model)
@@ -94,6 +95,106 @@ namespace MVC_Store.Areas.Admin.Controllers
             return RedirectToAction("Index");
 
         }
-    }   
+
+        // GET: Admin/Pages/EditPage/id
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+            // обьявляю модель PageVM
+            PageVM model;
+            using (Db db = new Db())
+            {
+                //получение страницы
+                PagesDTO dto = db.Pages.Find(id);
+                //проверка доступности страницы
+                if(dto == null)
+                {
+                    return Content("The page is not exist.");
+                }
+                //инициализация модели данными
+                model = new PageVM(dto);
+            }
+            //возвращаем модель в представление
+            return View(model);
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            //проверка модели на валидность
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                //получаем id страницы
+                int id = model.Id;
+                //обьявляю переменную для Slug
+                string slug = "home";
+                //получение страницы по id
+                PagesDTO dto = db.Pages.Find(id);
+                //присваиваем название из полученной модели в DTO
+                dto.Title = model.Title;
+                //проверка краткого заголовка и присваиваем если необходимо
+                if(model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+                //проверка slug и title на уникальность
+                if(db.Pages.Where(x=> x.Id != id).Any(x => x.Title == model.Title))
+                {
+                    ModelState.AddModelError("", "That title alredy exist.");
+                    return View(model);
+                }
+                else if (db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug)) 
+                {
+                    ModelState.AddModelError("", "That slug alredy exist.");
+                    return View(model);
+                }
+                //записываем остальные значения в класс DTO
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                //сохранение изменений в базу
+                db.SaveChanges();
+            }
+            //оповещение пользователя в TempData
+            TempData["SM"] = "You have edited the page.";
+            //переадресация пользователя
+            return RedirectToAction ("EditPage");
+        }
+
+        // GET: Admin/Pages/PageDetails/id
+        public ActionResult PageDetails(int id)
+        {
+            //обьяление модели PageVM
+            PageVM model;
+            using (Db db = new Db())
+            {
+                //получение страницы
+                PagesDTO dto = db.Pages.Find(id);
+                //проверка доступности страницы
+                if(dto == null)
+                {
+                    return Content("This page dors not exist.");
+                }
+                //присваиваем модели все поля из базы
+                model = new PageVM(dto);
+            }
+            //возвращаем модель в представление
+            return View(model);
+        }
+        
+    }
 
 }
